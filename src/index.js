7abd98dbccb8ee3765788e7c3e0d80c8abfcca82
Noemi Calizaya <calizayanoemiisabel@gmail.com>
2021-10-14 00:30:29 -0400

    branch

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ import { CounterApp } from './components/CounterApp';
 
 ReactDOM.render(<CounterApp />,document.getElementById('root'));
 //probando
+//tarea de backend
+// tarea taeraegvchbjknknm,mlmichi
